f9256336c6d9d21b5d060fd17d56c163b8170db4
wilis13 <wilis_bonser@yahoo.com>
2024-07-01 12:01:52 +0700

    ini file pertama

---
+++ latihan1.php
@@ -0,0 +1,11 @@
+<?php
+echo "HEll0 W0r1d"
+//perintah untuk add
+//1. git add . atau git add nama_folder/file
+//perintah untuk commit atau komentar
+//2. git commit -m "komentarnya apa"
+//perintah untuk upload atau push
+//3. git push -u origin master
+// push adalah perintah, origin adalah nama remote
+// master adalah nama branch
+?>
