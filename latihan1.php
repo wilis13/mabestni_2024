1f0ca6e323db8c61cc56d8ef4284100325b54b0c
wilis13 <wilis_bonser@yahoo.com>
2024-07-01 12:16:01 +0700

    ubah file

--- latihan1.php
+++ latihan1.php
@@ -7,5 +7,7 @@ echo "HEll0 W0r1d"
 //perintah untuk upload atau push
 //3. git push -u origin master
 // push adalah perintah, origin adalah nama remote
-// master adalah nama branch
+// main adalah nama branch
+//4. ini adalah  modify dari saya
+//5. dengan menambahkan file baru
 ?>
